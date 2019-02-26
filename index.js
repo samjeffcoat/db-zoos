5ccc4ba74533e5dcfee92351f51bb067e794275a
samjeffcoat <samjeffcoat@gmail.com>
2019-02-26 14:56:07 -0600

    Sets up project and server. Creates sqlite file

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
 
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.db"
+  },
+  useNullAsDefault: true
+};
+
+const db = knex(knexConfig);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// Post to Zoos
+server.post("/api/zoos", (req, res) => {
+  db("zoos")
+    .insert(req.body)
+    .then(ids => {
+      const [id] = ids;
+
+      db("zoos")
+        .where({ id })
+        .first()
+        .then(zoo => {
+          res.status(200).json(zoo);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
